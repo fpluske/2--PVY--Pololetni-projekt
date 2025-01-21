94d5b4716a7d918b2071bac359cefd8355918874
fpluske <frantisek.pluske@seznam.cz>
2025-01-21 20:29:21 +0100

    V1.1

--- script.js
+++ script.js
@@ -69,32 +69,46 @@ function update() {
         context.fillRect(hadBody[i][0], hadBody[i][1], velikostPlochy, velikostPlochy);
     }
 
-    if (hadX < 0 
-        || hadX > celkemSloupcu * velikostPlochy 
-        || hadY < 0 
-        || hadY > celkemRadku * velikostPlochy) { 
-        
+    if (hadX < 0
+        || hadX > celkemSloupcu * velikostPlochy
+        || hadY < 0
+        || hadY > celkemRadku * velikostPlochy) {
+
         konecHry = true;
-        zvukKonecHry.play();
-        alert("Konec hry");
-        location.reload();
+        konecObraz();
     }
 
     for (let i = 0; i < hadBody.length; i++) {
-        if (hadX == hadBody[i][0] && hadY == hadBody[i][1]) { 
-            
+        if (hadX == hadBody[i][0] && hadY == hadBody[i][1]) {
+
             konecHry = true;
-            zvukKonecHry.play();
-            alert("Konec hry");
-            location.reload();
+            konecObraz();
         }
     }
+}
+
+function textPresPole(text, velikost, barva, poziceX, poziceY) {
+    context.fillStyle = barva;
+    context.font = velikost;
+    context.fillText(text, poziceX, poziceY);
+}
+
+function konecObraz() {
+    let img = new Image();
+    img.src = 'img/konecHry.png';
+    img.onload = function () {
+        context.drawImage(img, 0, 0, board.width, board.height);
+        textPresPole("Konec hry!", "50px Arial", "red", board.width / 2 - 140, board.height / 2);
+        textPresPole(`Skóre: ${score}`, "40px Arial", "red", board.width / 2 - 80, board.height / 2 + 50);
+    };
+    zvukKonecHry.play();
 }
 
 function zmenaSmeru() {
     document.addEventListener('keydown', (event) => {
         const key = event.key.toLowerCase();
         switch (key) {
+            case 'w':
             case 'arrowup':
                 if (speedY == 0) {
                     speedX = 0;
@@ -102,6 +116,7 @@ function zmenaSmeru() {
                     pohybNahoru.play();
                 }
                 break;
+            case 's':
             case 'arrowdown':
                 if (speedY == 0) {
                     speedX = 0;
@@ -109,6 +124,7 @@ function zmenaSmeru() {
                     pohybDolu.play();
                 }
                 break;
+            case 'a':
             case 'arrowleft':
                 if (speedX == 0) {
                     speedX = -1;
@@ -116,36 +132,8 @@ function zmenaSmeru() {
                     pohybDoleva.play();
                 }
                 break;
-            case 'arrowright':
-                if (speedX == 0) {
-                    speedX = 1;
-                    speedY = 0;
-                    pohybDoprava.play();
-                }
-                break;
-
-            case 'w':
-                if (speedY == 0) {
-                    speedX = 0;
-                    speedY = -1;
-                    pohybNahoru.play();
-                }
-                break;
-            case 's':
-                if (speedY == 0) {
-                    speedX = 0;
-                    speedY = 1;
-                    pohybDolu.play();
-                }
-                break;
-            case 'a':
-                if (speedX == 0) {
-                    speedX = -1;
-                    speedY = 0;
-                    pohybDoleva.play();
-                }
-                break;
             case 'd':
+            case 'arrowright':
                 if (speedX == 0) {
                     speedX = 1;
                     speedY = 0;
@@ -159,6 +147,6 @@ function zmenaSmeru() {
 }
 
 function jidlo() {
-    jidloX = Math.floor(Math.random() * celkemSloupcu) * velikostPlochy; 
-    jidloY = Math.floor(Math.random() * celkemRadku) * velikostPlochy; 
+    jidloX = Math.floor(Math.random() * celkemSloupcu) * velikostPlochy;
+    jidloY = Math.floor(Math.random() * celkemRadku) * velikostPlochy;
 }
